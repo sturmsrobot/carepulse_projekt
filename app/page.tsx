4e78af2269a8d71b9b9c5714424422df4e214f57
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-08-05 22:26:33 +0200

    PatientForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,38 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen mac-h-screen">
+      {/* TODO: OTP-Verifizierung einfügen / PasskeyModel */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <Image />
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PatientForm = () => {
+  return <div>PatientForm</div>;
+};
+
+export default PatientForm;
