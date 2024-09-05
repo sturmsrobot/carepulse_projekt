ec772b81cb9e66c3cd1e8fec513fcadeffc56793
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-09-05 12:07:53 +0200

    User-Authentifizierung #4

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,12 +1,25 @@
+"use server";
+
 import { ID, Query } from "node-appwrite";
-import { users } from "../appwrite.config";
+
+import {
+  BUCKET_ID,
+  DATABASE_ID,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  PROJECT_ID,
+  databases,
+  storage,
+  users,
+} from "../appwrite.config";
+
 import { parseStringify } from "../utils";
 
 // CREATE APPWRITE USER
 export const createUser = async (user: CreateUserParams) => {
   try {
     // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
-    const newuser = await users.create(
+    const newUser = await users.create(
       ID.unique(),
       user.email,
       user.phone,
@@ -14,7 +27,7 @@ export const createUser = async (user: CreateUserParams) => {
       user.name
     );
 
-    return parseStringify(newuser);
+    return parseStringify(newUser);
   } catch (error: any) {
     // Check existing user
     if (error && error?.code === 409) {
--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,53 +1,25 @@
 import * as sdk from "node-appwrite";
 
-// export const {
-//   PROJECT_ID,
-//   API_KEY,
-//   DATABASE_ID,
-//   PATIENT_COLLECTION_ID,
-//   DOCTOR_COLLECTION_ID,
-//   APPOINTMENTS_COLLECTION_ID,
-//   NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
-//   NEXT_PUBLIC_ENDPOINT: ENDPOINT,
-// } = process.env;
-
-// console.log(DATABASE_ID);
-// const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
-// console.log(ENDPOINT);
-
-// export const PROJECT_ID = process.env.PROJECT_ID;
-// export const API_KEY = process.env.API_KEY;
-// export const DATABASE_ID = process.env.DATABASE_ID;
-// export const PATIENT_COLLECTION_ID = process.env.PATIENT_COLLECTION_ID;
-// export const DOCTOR_COLLECTION_ID = process.env.DOCTOR_COLLECTION_ID;
-// export const APPOINTMENTS_COLLECTION_ID =
-//   process.env.APPOINTMENTS_COLLECTION_ID;
-// export const BUCKET_ID = process.env.BUCKET_ID;
-// export const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
-
-export const PROJECT_ID = "66b2747b0027349ad3f3";
-export const API_KEY =
-  "6a33cfecfe1bbafc456f69edfb7609807b7c0635575adbad397622e8cdd558f9f9503c1ead0032edb4b97289229b2654fb54da7399936f9ced2529657eb256d9b51c6f5f442fc8e9bd7dd612a4b2263156465b2497f087765c610c9ed005333a8fa30457cc0f982f11397fbac95610f4cdd3909a8f9ee3e69ad6761b77e653009ced2529657eb256d9b51c6f5f442fc8e9bd7dd612a4b2263156465b2497f087765c610c9ed005333a8fa30457cc0f982f11397fbac95610f4cdd3909a8f9ee3e69ad6761b77e65300";
-export const DATABASE_ID = "66b2753b001c4d25ab7d";
-export const PATIENT_COLLECTION_ID = "66b27577001e2e26de69";
-export const DOCTOR_COLLECTION_ID = "66b275ac002eaf511db1";
-export const APPOINTMENTS_COLLECTION_ID = "66b275d2002c70f79c91";
-export const BUCKET_ID = "66b2761400281151725f";
-export const ENDPOINT = "https://cloud.appwrite.io/v1";
-
-console.log(API_KEY);
-console.log(PROJECT_ID);
-
-export const corsOptions = {
-  origin: "localhost:3000",
-  optionsSuccessStatus: 200,
-};
+export const {
+  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+  NEXT_PUBLIC_PROJECT_ID: PROJECT_ID,
+  NEXT_PUBLIC_API_KEY: API_KEY,
+  NEXT_PUBLIC_DATABASE_ID: DATABASE_ID,
+  NEXT_PUBLIC_PATIENT_COLLECTION_ID: PATIENT_COLLECTION_ID,
+  NEXT_PUBLIC_DOCTOR_COLLECTION_ID: DOCTOR_COLLECTION_ID,
+  NEXT_PUBLIC_APPOINTMENTS_COLLECTION_ID: APPOINTMENT_COLLECTION_ID,
+  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
+} = process.env;
 
 const client = new sdk.Client();
 
 client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
 
 export const databases = new sdk.Databases(client);
-export const storage = new sdk.Storage(client);
-export const messaging = new sdk.Messaging(client);
 export const users = new sdk.Users(client);
+export const messaging = new sdk.Messaging(client);
+export const storage = new sdk.Storage(client);
+
+console.log(process.env.NEXT_PUBLIC_ENDPOINT);
+console.log(process.env.NEXT_PUBLIC_PROJECT_ID);
+console.log(process.env.NEXT_PUBLIC_API_KEY);
